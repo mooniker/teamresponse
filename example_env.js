06f35036165650ca74e3d4624843f795b64e3f00
James Moon <mooniker@gmail.com>
2016-03-23 12:02:18 -0400

    Add example for env.js config variables

---
+++ example_env.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PORT: 4000,
+  MONGO_SERVER_URI: 'mongodb://localhost/teamresponse',
+  ADMIN_USERNAME: 'admin',
+  ADMIN_PASSWORD: 'notsecure',
+  TO_EMAIL: 'someone@somewhere.com'
+};
